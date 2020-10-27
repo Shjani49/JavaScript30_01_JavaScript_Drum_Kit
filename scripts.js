c998bd7b0cefaef889dd7920ab359da6f9a5cc39
Shjani49 <shjani49@gmail.com>
2020-10-27 15:20:03 -0600

    debounce function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 // JavaScript source code
+// @ Link: https://github.com/wesbos/JavaScript30/blob/master/13%20-%20Slide%20in%20on%20Scroll/index-FINISHED.html
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
