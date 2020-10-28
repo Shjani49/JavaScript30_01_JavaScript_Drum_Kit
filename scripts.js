5525dc9500a00714ad0b9ef13352d47936541d9a
Shjani49 <shjani49@gmail.com>
2020-10-28 16:52:23 -0600

    Select All  the link on the page;  create and  highlight class; Append with body; trigger and highlight all the link in to page;

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-﻿
+﻿const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+
+highlight.classList.add('highlight');
+document.body.appendChild(highlight);
+
+function highlightLink() {
+    console.log(this);
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
