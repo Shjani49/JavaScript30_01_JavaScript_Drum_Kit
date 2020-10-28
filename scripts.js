ba3394dc0e8dcf1ef1c0c0fe0143b1a2dab165dd
Shjani49 <shjani49@gmail.com>
2020-10-28 16:42:04 -0600

    Initial Commit; Create Template Files;

--- scripts.js
+++ scripts.js
@@ -1,11 +1,1 @@
-﻿// @ Link: https://github.com/wesbos/JavaScript30/blob/master/21%20-%20Geolocation/index-FINISHED.html
-const arrow = document.querySelector('.arrow');
-const speed = document.querySelector('.speed-value');
-
-navigator.geolocation.watchPosition((data) => {
-    console.log(data);
-    speed.textContent = data.coords.speed;
-    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
-}, (err) => {
-    console.error(err);
-});
+﻿
