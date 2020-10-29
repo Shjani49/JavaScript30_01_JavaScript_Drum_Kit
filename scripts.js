21a6e2085ec5a4f33c64956540ec567ce14a521a
Shjani49 <shjani49@gmail.com>
2020-10-29 16:28:17 -0600

    Add peep() function for random time and hole up

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 ﻿const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
+let timeUp = false;
+let score = 0;
 
 function randomTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
@@ -9,6 +12,20 @@ function randomTime(min, max) {
 function randomHole(holes) {
     const idx = Math.floor(Math.random() * holes.length);
     const hole = holes[idx];
-    
+    if (hole === lastHole) {
+        console.log('Ah nah thats the same one bud');
+        return randomHole(holes);
+    }
+    lastHole = hole;
     return hole;
+}
+
+function peep() {
+    const time = randomTime(200, 1000);// We change the random time for different level of game; For Expert level: we can set the randomTime(50,300);
+    const hole = randomHole(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove('up');
+        if (!timeUp) peep();
+    }, time);
 }
