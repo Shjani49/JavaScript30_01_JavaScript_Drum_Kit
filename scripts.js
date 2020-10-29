cd07bfd277c3b2512ac8918da3e18c35d606cb40
Shjani49 <shjani49@gmail.com>
2020-10-29 15:39:09 -0600

    Add Displa end-time and Start-time functions in Js file

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 ﻿let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
     // clear any existing timers
@@ -8,6 +9,7 @@ function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
+    displayEndTime(then);
 
     countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -27,4 +29,17 @@ function displayTimeLeft(seconds) {
     const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
     document.title = display;
     timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
 }
